f93cae5b4d47d36e20a5c7897cac3526672c741a
abejarano <programador.angel@gmail.com>
2025-10-01 08:08:20 -0300

    refactor: enhance security configurations in SecurityModule

--- src/modules/SecurityModule.ts
+++ src/modules/SecurityModule.ts
@@ -6,12 +6,31 @@ export class SecurityModule extends BaseServerModule {
   name = "Security";
   priority = -80;
 
-  private helmetOptions: Parameters<typeof helmet>[0];
+  private readonly helmetOptions: Parameters<typeof helmet>[0];
 
   constructor(helmetOptions?: Parameters<typeof helmet>[0]) {
     super();
     this.helmetOptions = helmetOptions || {
-      xXssProtection: true,
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          styleSrc: ["'self'", "'unsafe-inline'"],
+          scriptSrc: ["'self'"],
+          imgSrc: ["'self'", "data:", "https:"],
+        },
+      },
+      crossOriginEmbedderPolicy: true,
+      crossOriginOpenerPolicy: true,
+      crossOriginResourcePolicy: { policy: "cross-origin" },
+      dnsPrefetchControl: true,
+      frameguard: { action: "deny" },
+      hidePoweredBy: true,
+      ieNoOpen: true,
+      noSniff: true,
+      originAgentCluster: true,
+      permittedCrossDomainPolicies: false,
+      referrerPolicy: { policy: "no-referrer" },
+      xssFilter: true,
     };
   }
 
